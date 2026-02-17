154c4405c696f9820cb8aaf720b8cf6f64dc5870
Dar Zaid <techzaid2021@gmail.com>
2026-02-17 14:46:26 +0530

    Added complete practice for Kotlin variables

---
+++ variables-complete.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val name = "Dar Zaid"
+    var age = 22
+    val height = 5.8
+    val grade: Char = 'A'
+    val isStudent = true
+
+    println("Name: $name")
+    println("Age: $age")
+    println("Height: $height")
+    println("Grade: $grade")
+    println("Student: $isStudent")
+
+    age = 23
+    println("Next year age: $age")
+}
